c9afc07071d4a2f23ef2a5078ddded74fd147ffc
Leandro Zanchetta <leandronetoz@hotmail.com>
2022-12-27 17:57:18 -0300

    Classe de inicialização

---
+++ src/main/java/com/zanchetta/Projeto2/config/Initiation.java
@@ -0,0 +1,29 @@
+package com.zanchetta.Projeto2.config;
+
+import com.zanchetta.Projeto2.entities.User;
+import com.zanchetta.Projeto2.repository.UserRepository;
+import com.zanchetta.Projeto2.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class Initiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+}
--- src/main/java/com/zanchetta/Projeto2/resources/UserResource.java
+++ src/main/java/com/zanchetta/Projeto2/resources/UserResource.java
@@ -1,9 +1,14 @@
 package com.zanchetta.Projeto2.resources;
 
+import com.zanchetta.Projeto2.entities.User;
 import com.zanchetta.Projeto2.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -13,7 +18,10 @@ public class UserResource {
     private UserService userService;
 
 
-
-
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
 
 }
